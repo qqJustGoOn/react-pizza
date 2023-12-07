9d2f1deeb9f65823c1f5b559141146b70124a9c8
qqJustGoOn <66961636+qqJustGoOn@users.noreply.github.com>
2023-12-07 23:20:39 +0300

    getting pizzas using createAsyncThunk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,8 @@ import {PizzaBlock} from "../components/PizzaBlock";
 import Pagination from "../components/Pagination";
 import {SearchContext} from "../App";
 import {setCategoryId, setCurrentPage, setFilters} from "../redux/slices/filterSlice";
+import {fetchPizzas} from "../redux/slices/pizzaSlice";
+import styles from "../components/NotFoundBlock/NotFoundBlock.module.scss";
 
 const Home = () => {
     const navigate = useNavigate();
@@ -19,12 +21,11 @@ const Home = () => {
     const isMounted = React.useRef(false);
 
     const {categoryId, sort, currentPage} = useSelector((state) => state.filter);
+    const {items, status} = useSelector((state) => state.pizza);
     // const { categoryId, currentPage, sort: { sortProperty: sortType } } = useSelector(state => state.filter)
     const sortType = sort.sortProperty;
 
     const {searchValue} = React.useContext(SearchContext);
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
     const [orderType, setOrderType] = React.useState(true);
 
 
@@ -35,21 +36,26 @@ const Home = () => {
         dispatch(setCurrentPage(number));
     }
 
-    const fetchPizzas = () => {
-        setIsLoading(true);
+    const getPizzas = async () => {
+
 
         const category = categoryId > 0 ? `category=${categoryId}` : '';
         const search = searchValue ? `&search=${searchValue}` : '';
         const sortBy = sortType.replace('-', '');
         const order = sortType.includes('-') ? 'asc' : 'desc';
 
-        axios.get(`https://6538192ea543859d1bb13d9d.mockapi.io/items?page=${currentPage}&limit=4&${
-            category}&sortBy=${sortBy}&order=${order}${search}`)
-            .then((res) => {
-                setItems(res.data);
-                setIsLoading(false);
-            });
+        //Получение пицц используя createAsyncThunk
+        dispatch(
+            fetchPizzas({
+                category,
+                search,
+                sortBy,
+                order,
+                currentPage,
+            })
+        );
 
+        window.scrollTo(0, 0);
     }
     // Если был первый рендер, то проверяем URl-параметры и сохраняем в редуксе
     React.useEffect(() => {
@@ -90,12 +96,11 @@ const Home = () => {
         window.scrollTo(0, 0);
 
         if (!isSearch.current) {
-            fetchPizzas();
+            getPizzas();
         }
 
         isSearch.current = false;
     }, [categoryId, sortType, searchValue, currentPage]);
-
 
 
     // const pizzas = items.filter((obj) => obj.title.toLowerCase().includes(searchValue.toLowerCase())
@@ -107,16 +112,25 @@ const Home = () => {
         <div className="container">
             <div className="content__top">
                 <Categories categoryId={categoryId} changeCategory={(id) => onSetCategory(id)}/>
-                <Sort  orderType={orderType} setOrderType={setOrderType}/>
+                <Sort orderType={orderType} setOrderType={setOrderType}/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {
-                    isLoading
-                        ? skeletons
-                        : pizzas
-                }
-            </div>
+            {status === 'error' ? (
+                <div className="content__error-info">
+                    <h2> 😕 Произошла ошибка 😕
+                    </h2>
+                    <p>К сожалению пиццы не найдены</p>
+                </div>
+            ) : (
+                <div className="content__items">
+                    {
+                        status === 'loading'
+                            ? skeletons
+                            : pizzas
+                    }
+                </div>
+            )}
+
             <Pagination currentPage={currentPage} onChangePage={onChangePage}/>
         </div>
     );
---
+++ src/redux/slices/pizzaSlice.js
@@ -0,0 +1,49 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import axios from "axios";
+
+// Cоздаем `thunk`
+export const fetchPizzas = createAsyncThunk(
+    'pizza/fetchPizzas',
+    async (params) => {
+        const {category, search, sortBy, order, currentPage} = params;
+        const {data} = await axios.get(`https://6538192ea543859d1bb13d9d.mockapi.io/items?page=${currentPage}&limit=4&${
+            category}&sortBy=${sortBy}&order=${order}${search}`);
+        return data;
+    }
+);
+
+const initialState = {
+    items: [],
+    status: 'loading', //loading | success | error
+}
+
+export const pizzaSlice = createSlice({
+    name: 'pizza',
+    initialState,
+    reducers: {
+        setItems(state, {payload}) {
+            state.items = payload;
+        },
+    },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchPizzas.pending, (state) => {
+                state.status = 'loading';
+                state.items = [];
+            })
+            .addCase(fetchPizzas.fulfilled, (state, action) => {
+                state.items = action.payload;
+                state.status = 'success';
+            })
+            .addCase(fetchPizzas.rejected, (state) => {
+                state.status = 'error';
+                state.items = [];
+            })
+    }
+});
+
+// Action creators are generated for each case reducer function
+export const {setItems} = pizzaSlice.actions;
+
+export default pizzaSlice.reducer;
